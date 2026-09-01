6490a6acfa6eff39a90fe09b245468e5f4d16b0e
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2026-09-01 18:10:40 -0300

    feat(projects): setup routes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import UnderConstructionWelcome from './components/UnderConstructionWelcome.vue'
 </script>
 
 <template>
@@ -8,10 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <UnderConstructionWelcome msg="Pardon the dust." />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/projects">Projects</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="projects">
+    <h1>This is the projects page</h1>
+  </div>
+  <div>
+    <h2>Project {{ $route.params.slug }}</h2>
+  </div>
+</template>
