e2988f4406a4314b9fa643a928df7522a41a7168
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2026-09-11 19:13:19 -0300

    build(components): create ProjectCard that displays info specific for project

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AppLink from '@/components/AppLink.vue'
+import type { Project } from '@/types/project'
+
+interface Props {
+  projectInfo: Project
+}
+
+const props = defineProps<Props>()
+const { approach, date, problem, repoUrl, retrospective, slug, summary, title } = props.projectInfo
+const projectPath = `/project/${slug}`
+</script>
+
+<template>
+  <article>
+    <h2>
+      <AppLink :to="projectPath">
+        {{ title }}
+      </AppLink>
+    </h2>
+    <h3>{{ summary }}</h3>
+    <hr />
+    <p>Problem: {{ problem }}</p>
+    <hr />
+    <p>Approach: {{ approach }}</p>
+    <hr />
+    <p>Retrospective: {{ retrospective }}</p>
+    <hr />
+    <p>Date: {{ date }}</p>
+    <AppLink class="bg-sky-500 hover:bg-sky-700" :to="repoUrl"> Github Repo </AppLink>
+  </article>
+</template>
