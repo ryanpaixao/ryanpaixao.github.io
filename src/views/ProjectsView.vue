781e2dd2b2aaf2304a2f87214ad0b0059d0e56eb
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2026-09-13 12:19:58 -0300

    test(playwright): update Projects path per Projects spec

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,9 +2,6 @@
 
 <template>
   <div class="projects">
-    <h1>This is the projects page</h1>
-  </div>
-  <div>
-    <h2>Project {{ $route.params.slug }}</h2>
+    <h1>Projects</h1>
   </div>
 </template>
