a0d9d63439a9481c73eb38a14173c5ebffa70a2f
Ryan Paixao <16196485+ryanpaixao@users.noreply.github.com>
2026-09-14 16:01:23 -0300

    style(MainNav): add simple styling to MainNav

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/projects">Projects</RouterLink>
-    <RouterLink to="/about">About</RouterLink>
+  <nav class="main-nav bg-green-500">
+    <RouterLink class="main-nav-button" to="/">Home</RouterLink>
+    <RouterLink class="main-nav-button" to="/projects">Projects</RouterLink>
+    <RouterLink class="main-nav-button" to="/about">About</RouterLink>
   </nav>
 </template>
+
+<style>
+.main-nav {
+  margin-left: 4px !important;
+}
+.main-nav-button {
+  padding: 8px;
+  background-color: lime;
+  border: solid green 0.5px;
+}
+</style>
